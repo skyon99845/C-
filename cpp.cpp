8ded33d165bb961a7f1df5bb5b44441799c7c6a2
ShubhamKumar <skyon9801@gmail.com>
2022-01-17 02:44:53 -0500

    uploaded

---
+++ cpp.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Area(l,b) (l*b)
+//void functionWritting(a,b);
+
+
+
+void functionWritting(int a,int b)
+{
+	cout<<"We are in the writting function"<<endl;
+
+}
+
+
+
+
+
+
+
+//Driver Function
+int main()
+{
+	int a,b;
+	cout<<"Enter the vale of a"<<endl;
+	cin>>a;
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+	functionWritting(a,b);
+	return 0;
+}
